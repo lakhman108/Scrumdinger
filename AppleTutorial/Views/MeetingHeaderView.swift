23fac903757ee73e9a12aa46063f990c7c6e0ddd
lakhman108 <119241790+lakhman108@users.noreply.github.com>
2025-08-17 01:01:27 +0530

    Create an overlay view and Extract the meeting header

---
+++ AppleTutorial/Views/MeetingHeaderView.swift
@@ -0,0 +1,18 @@
+//
+//  MeetingHeaderView.swift
+//  AppleTutorial
+//
+//  Created by parmar lakhman on 17/08/25.
+//
+
+import SwiftUI
+
+struct MeetingHeaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MeetingHeaderView()
+}
