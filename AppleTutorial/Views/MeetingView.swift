4f3cf829020f6434355b6c90189ead2239a99c3a
Parmar Lakhman <119241790+lakhman108@users.noreply.github.com>
2025-11-27 20:35:47 +0530

    Integrate TimerKit and add meeting UI components
    Added TimerKit as a dependency and updated MeetingView to use ScrumTimer for meeting progress. Introduced MeetingHeaderView and MeetingFooterView for displaying meeting status and speaker info, and refactored DetailView to pass scrum binding to MeetingView. Improved meeting UI with theme support and accessibility enhancements.

--- AppleTutorial/Views/MeetingView.swift
+++ AppleTutorial/Views/MeetingView.swift
@@ -6,37 +6,41 @@
 //
 
 import SwiftUI
+import TimerKit
+
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    @State var scrumTimer = ScrumTimer()
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 10,total: 15)
-            HStack{
-                VStack(alignment: .leading){
-                    Text("Seconds Elapsed")
-                        .font(.caption)
-                    Label("300",systemImage: "hourglass.tophalf.fill")
-                }
-                Spacer()
-                VStack(alignment: .trailing){
-                    Text("Seconds Remaining")
-                        .font(.caption)
-                    Label("600",systemImage: "hourglass.bottomhalf.fill")
-                }
-            }
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            
-            HStack{
-                Text("speaker 1 of 3")
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
-                }
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(scrum.theme.mainColor)
+            VStack {
+                MeetingHeaderView(secondsElapsed: scrumTimer.secondsElapsed,
+                                  secondsRemaining: scrumTimer.secondsRemaining,
+                                  theme: scrum.theme)
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                MeetingFooterView(speakers: scrumTimer.speakers, skipAction: scrumTimer.skipSpeaker)
             }
         }
+        .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .onAppear{
+            scrumTimer.reset(lengthInMinutes: scrum.lengthInMinutes, attendeeNames: scrum.attendees.map {$0.name} )
+            scrumTimer.startScrum()
+        }
+                .onDisappear{
+                    scrumTimer.stopScrum()
+                }
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
+
 #Preview {
-    MeetingView()
+    @Previewable @State var scrum = DailyScrum.sampleData[0]
+    MeetingView(scrum: $scrum)
 }
